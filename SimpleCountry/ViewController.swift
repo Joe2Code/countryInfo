63518fcdee1b0e582728b4573229c48295fd2be4
Joseph Mennemeier <jmennemeier@aol.com>
2016-05-27 19:41:33 -0500

    Initial Commit

---
+++ SimpleCountry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCountry
+//
+//  Created by Joseph Mennemeier on 5/27/16.
+//  Copyright © 2016 Joseph Mennemeier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
